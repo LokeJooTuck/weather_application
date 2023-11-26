4c5785a7acdd7cbcaaf69d4794ef18d38b2296e3
See Pao Yeong <seepaokeong@gmail.com>
2023-11-26 21:11:19 +0800

    Update weather_api_provider.dart

--- lib/data/source/weather_api_provider.dart
+++ lib/data/source/weather_api_provider.dart
@@ -10,7 +10,7 @@ class WeatherApiProvider {
   Future<Weather> fetchWeather(double latitude, double longitude) async {
     final response = await http.get(
       Uri.parse(
-          'https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=relative_humidity_2m&daily=weather_code,temperature_2m_max,apparent_temperature_max,wind_speed_10m_max&forecast_days=4'),
+          'https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=relative_humidity_2m&daily=weather_code,temperature_2m_max,apparent_temperature_max,wind_speed_10m_max&forecast_days=7'),
     );
 
     if (response.statusCode == 200) {
