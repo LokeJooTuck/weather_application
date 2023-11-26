1ddc8617b33fa387eec403f63e1574b69893272d
jootuckloke <s58101@ocean.umt.edu.my>
2023-11-26 18:02:35 +0800

    File additions
    Added repository file, modified location_service file, added toast package

--- lib/data/source/weather_api_provider.dart
+++ lib/data/source/weather_api_provider.dart
@@ -1,25 +1,32 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/weather.dart';
 
 class WeatherApiProvider {
   Future<Weather> fetchWeather(double latitude, double longitude) async {
-    latitude = 2.5;
-    longitude = 112.5;
     final response = await http.get(
       Uri.parse(
           'https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=relative_humidity_2m&daily=weather_code,temperature_2m_max,apparent_temperature_max,wind_speed_10m_max&forecast_days=4'),
     );
 
     if (response.statusCode == 200) {
-      return Weather.fromJson(
+      final weather = Weather.fromJson(
           latitude: latitude.toString(),
           longitude: longitude.toString(),
           locationAreaName: 'Malaysia',
           weatherAPIResponseJsonObject: jsonDecode(response.body) as Map<String, dynamic>);
+
+      return weather;
     } else {
+      Fluttertoast.showToast(
+          msg: "Weather fetch failed. Try again.",
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
       throw Exception('Weather fetch failed. Try again.');
     }
   }
--- lib/utils/services/location_service.dart
+++ lib/utils/services/location_service.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-Future<bool> _handleLocationPermission(BuildContext context) async {
+Future<bool> handleLocationPermission() async {
   bool serviceEnabled;
   LocationPermission permission;
 
   serviceEnabled = await Geolocator.isLocationServiceEnabled();
   if (!serviceEnabled) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(const SnackBar(content: Text('Location services are disabled. Please enable the services')));
-    return false;
-  }
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
+    // ScaffoldMessenger.of(context)
+    //     .showSnackBar(const SnackBar(content: Text('Location services are disabled. Please enable the services')));
+    // return false;
+
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Location permissions are denied')));
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //     const SnackBar(content: Text('Location permissions are permanently denied, we cannot request permissions.')));
       return false;
     }
-  }
-  if (permission == LocationPermission.deniedForever) {
-    ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Location permissions are permanently denied, we cannot request permissions.')));
-    return false;
+    return true;
   }
   return true;
 }
 
-Future<void> _getCurrentPosition(BuildContext context) async {
-  final hasPermission = await _handleLocationPermission(context);
+Future<void> getCurrentPosition(var currentPosition) async {
+  final hasPermission = await handleLocationPermission();
   if (!hasPermission) return;
-  await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
-      .then((Position position) {})
-      .catchError((e) {
+  await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then((Position position) {
+    currentPosition = position;
+    print('lat long');
+    print(currentPosition?.longitude);
+    print(currentPosition?.latitude);
+  }).catchError((e) {
     debugPrint(e);
   });
 }
